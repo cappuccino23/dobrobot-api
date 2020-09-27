63e6165312b2e429115f813f681f5ab4e7aed814
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-09-27 23:42:59 +0300

    update project

--- models/__init.py__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/payments_dobrobot'
-engine = create_engine("postgresql+psycopg2://dobrobot:dobrobot@/payments_dobrobot")
-
-db = SQLAlchemy(app)
