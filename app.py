e7151b3d3243fbfbf11278d934b9a71663ba6632
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-09-27 23:39:57 +0300

    update project

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, jsonify, request
+import psycopg2
+from flask import Flask, request
+
+from Dobrobot.pay_initiation import init_pay
+
+from Dobrobot.result import add_res_data
+
 app = Flask(__name__)
+
+conn = psycopg2.connect(dbname='payments_dobrobot', user='dobrobot', password='dobrobot', host='localhost')
 
 
 @app.route('/add', methods=['POST'])
 def add():
-    request_data = request.get_json()
-    return jsonify(request_data)
+    in_data = request.get_json()
+    init_pay(in_data)
+
+    return 'ok'
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    res_data = request.get_json()
+    try:
+
+        add_res_data(res_data)
+
+        return {"code": 0, "message": "Операция успешно проведена"}
+
+    except ValueError:
+        return 'Not OK'
 
 
 if __name__ == '__main__':
